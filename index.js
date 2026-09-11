24ff7aca9d1c7895e1bba452cd6e414405b3e77a
yoozenno <ilam89033@gmail.com>
2026-09-11 09:01:16 +0300

    feat: Add event-driven serer,logger

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const http = require('http');
+const EventEmitter = require('events');
+const logger = require('./logger');
+class AppServer extends EventEmitter {
+  start(port) {
+    this.server = http.createServer((req, res) => {
+      this.emit('request:received', { url: req.url, method: req.method });
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/html;charset=utf8'});
-    res.write('<h1>Матусевич Илья Михайлович</h1>');
-    res.write('<h1>Группа 478</h1>');
-    const p1 = Math.PI.toFixed(14);
-    res.write(`<h1>Число пи: ${p1}</h1>`);
+      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.write('Матусевич Илья Михайлович');
+      res.write('Группа 478');
+      res.write('Hello from Event-Driven Server!');
+    });
+
+    this.server.listen(port, () => {
+      this.emit('server:started', port);
+    });
+  }
+
+  stop() {
+    if (this.server) {
+      this.server.close(() => {
+        this.emit('server:stopped');
+      });
+    }
+  }
+}
+
+const app = new AppServer();
+
+app.on('server:started', (port) => {
+  console.log(`  Сервер запущен на порту ${port}`);
 });
 
-const PORT = 3000;
-server.listen(PORT, ()=>{
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+app.on('request:received', (reqInfo) => {
+  console.log(`  Получен запрос: ${reqInfo.method} ${reqInfo.url}`);
 });
+
+app.on('server:stopped', () => {
+  console.log('  Сервер остановлен');
+});
+
+logger.setupLogger(app);
+app.start(3000);
+
+setTimeout(() => {
+  app.stop();
+}, 10000);
